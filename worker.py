684994ad9985f48cec229ed4819a13c6632820c3
ApthsN <aparpat.hat@gmail.com>
2025-05-25 01:52:50 +0700

    Update worker.py

--- worker.py
+++ worker.py
@@ -1,9 +1,18 @@
-#worker.py
+# worker.py
 from celery import Celery
+import requests
 import os
 
 celery_app = Celery("tasks", broker=os.getenv("REDIS_URL", "redis://redis:6379/0"))
 
 @celery_app.task
-def dummy_task(message):
-    return f"รับข้อความจากผู้ใช้: {message}"
+def ask_model_task(prompt):
+    try:
+        response = requests.post(
+            f"{os.getenv('OLLAMA_URL', 'http://localhost:11434')}/api/generate",
+            json={"model": "llama3", "prompt": prompt, "stream": False}
+        )
+        data = response.json()
+        return data.get("response", "⚠️ ไม่พบคำตอบ")
+    except Exception as e:
+        return f"❌ เกิดข้อผิดพลาด: {e}"
